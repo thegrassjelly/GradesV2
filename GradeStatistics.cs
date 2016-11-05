bee4a45f2a88c819de9735b6d72da6253d2d509b
Robert Steven Tomas <steven.tomas@live.com>
2016-11-05 17:21:00 +0800

    Init

---
+++ GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿namespace GradesV2
+{
+    class GradeStatistics
+    {
+       public float AverageGrade;
+       public float HighestGrade;
+       public float LowestGrade;
+
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(Math.Round(stats.AverageGrade));
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            Console.ReadLine();
+        }
+    }
+}
