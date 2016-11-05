d91563a1e59e1198cc9c42681587f2bf92288bb3
Robert Steven Tomas <steven.tomas@live.com>
2016-11-06 01:00:16 +0800

    Types

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -1,11 +1,86 @@
-﻿using GradesV2;
+﻿using System;
+using GradesV2;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Grades.Tests.Types
 {
     [TestClass]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scoot";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOOT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date =  new DateTime(2015, 01, 01);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+
+        [TestMethod]
+        public void ValTypesPassByVal()
+        {
+            int x = 46;
+            IncNum(x);
+
+            Assert.AreEqual(x, 46);
+        }
+
+        private void IncNum(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void RefTypesPassByVal()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringComp()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+
+            bool result = String.Equals(name1, name2, 
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+
         [TestMethod]
         public void IntVarHoldVal()
         {
